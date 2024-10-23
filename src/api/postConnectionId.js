3e264c9708663d07ae5b97446d6c15a803e3f8e8
KatherineDG <katherinedomeqg@gmail.com>
2024-10-23 11:12:07 -0300

    cambio endpoint

--- src/api/postConnectionId.js
+++ src/api/postConnectionId.js
@@ -14,7 +14,7 @@ const postConnectionId = async (connectionId) => {
         mode: 'cors',
     };
 
-    const response = await fetch('http://localhost:5000/api/connection-id/front', requestOptions);
+    const response = await fetch('https://eda-daii-production-9f47.up.railway.app/api/connection-id/front', requestOptions);
     let jsonData = await response.json();
     return jsonData;
 }
