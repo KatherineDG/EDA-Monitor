19f1bb84fd148de7106ae852b3bd5dfbe7d1d053
KatherineDG <katherinedomeqg@gmail.com>
2024-11-02 12:37:21 -0300

    proxy cambios

--- src/api/getLogs.js
+++ src/api/getLogs.js
@@ -9,11 +9,10 @@ const getLogs = async () => {
         mode: 'cors',
     };
 
-    const response = await fetch(`http://ec2-3-89-66-61.compute-1.amazonaws.com:5000/api/logs`, requestOptions);
+    const response = await fetch(`http://ec2-54-90-250-142.compute-1.amazonaws.com:5000/api/logs`, requestOptions);
      // Intentar parsear la respuesta como JSON
     const data = await response.json();
     return data;
-
 }
 
 export default getLogs;
